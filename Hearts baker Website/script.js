a99ed1dcb1e5403b03779b724e0ace7977816d4e
Divanshu Sharma <sharmadivanshu007@gmail.com>
2023-06-16 00:34:36 +0530

    first commit

---
+++ Hearts baker Website/script.js
@@ -0,0 +1,15 @@
+
+ function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  
+function fadeOut(){
+    setInterval(loader, 1000);
+  }
+  
+  window.onload = fadeOut();
+
+
+
+  // Screen top
+ 
